36965f452f8a069cf094c3cdca32ef7acda86f57
Rahul <developer.rahul9888@gmail.com>
2024-07-19 13:47:39 +0530

    save changes

--- app/Models/UserDetail.php
+++ app/Models/UserDetail.php
@@ -42,6 +42,7 @@ class UserDetail extends Model
         'leave_approving_code',
         'department',
         'job_role',
+        'position',
         'grade',
         'employment_type',
         'aadhar_number',
